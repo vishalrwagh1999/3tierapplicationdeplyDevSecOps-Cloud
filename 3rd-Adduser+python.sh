b80f5ac907dace25451722cb59273aaad7989501
vishal wagh <vishalrwagh1999@gmail.com>
2026-04-16 09:07:12 +0530

    Add files via upload

---
+++ 3rd-Adduser+python.sh
@@ -0,0 +1,16 @@
+# Add jenkins user to docker group
+sudo usermod -aG docker jenkins
+
+# Restart Jenkins to apply changes
+sudo systemctl restart jenkins
+
+# Verify Jenkins can access Docker
+sudo -u jenkins docker ps
+
+# Update & Install Python pip 
+sudo apt-get update
+sudo apt-get install -y python3-venv python3-pip
+
+# Install docker-compose on your Jenkins server:
+sudo apt-get update
+sudo apt-get install -y docker-compose-plugin
